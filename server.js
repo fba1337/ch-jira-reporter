89bef490006b8a528c6e6e91e9fa898c5d887cf4
Felix Ballweg <felix.ballweg@colehaan.com>
2023-12-14 21:02:45 -0500

    ascii reporting

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 const sqlite3 = require('sqlite3').verbose();
+const asciichart = require('asciichart');
 
 const db = new sqlite3.Database('./db.sqlite', (err) => {
     if (err) {
@@ -36,10 +37,12 @@ fs.createReadStream('input/G3 - Defects (JIRA).csv')
         issues.push(row);
     })
     .on('end', () => {
-        let statuses = ['In Progress - Vendor', 'In Progress', 'In QA', 'To Do', 'Prod Validation', 'On Hold'];
+        let statuses = ['In Progress - Vendor', 'In Progress', 'In QA', 'To Do', 'Prod Validation', 'On Hold', 'Ready for release' ];
+        let priorities = ['Highest', 'High', 'Medium', 'Low' 
+    ]
         console.log('CSV file successfully processed');
         console.log('Number of issues total: ' + issues.filter(issue => statuses.includes(issue['Status'])).length);
-
+        
         statuses.forEach(status => {
             let count = issues.filter(issue => issue['Status'] === status).length;
             if (count > 0) {
@@ -47,13 +50,28 @@ fs.createReadStream('input/G3 - Defects (JIRA).csv')
             }
         });
 
-        let doneCount = issues.filter(issue => issue['Status'] === 'Done').length;
+        priorities.forEach(priority => {
+            let count = issues.filter(issue => issue['Priority'] === priority).length;
+            if (count > 0) {
+                console.log(`Number of issues with priority "${priority}": ${count}`);
+            }
+        });
 
+        let doneCount = issues.filter(issue => issue['Status'] === 'Done').length;
         let db = new sqlite3.Database('./db.sqlite', sqlite3.OPEN_READWRITE, (err) => {
             if (err) {
                 console.error(err.message);
             }
         });
+
+        let statusCounts = statuses.map(status => issues.filter(issue => issue['Status'] === status).length);
+        let priorityCounts = priorities.map(priority => issues.filter(issue => issue['Priority'] === priority).length);
+
+        console.log('Issues by status: ');
+        console.log(asciichart.plot(statusCounts, { height: 10 }));
+        console.log('Issues by priority: ');
+        console.log(asciichart.plot(priorityCounts, { height: 10 }));
+        
 
         db.get('SELECT value FROM Counts WHERE name="done"', (err, row) => {
             if (err) {
---
+++ test.js
@@ -0,0 +1,7 @@
+
+function add(a = 0,b = 0){
+    return a + b;
+    }
+
+ console.log(add(2,3));
+ console.log(add());
